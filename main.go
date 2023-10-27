fa550d7e12d011910c691044be7166af76e663db
Ben H <46547251+bnreplah@users.noreply.github.com>
2023-10-27 08:13:03 -0700

    Update main.go

--- main.go
+++ main.go
@@ -1,144 +1,108 @@
 package main
 
 import (
-	"fmt"
-	"log"
+	"flag"
+	"net"
+	"net/http"
 	"os"
-	"strings"
-	"time"
+	"os/exec"
 
-	"github.com/brian1917/vcodeapi"
+	"github.com/labstack/echo/middleware"
+
+	"github.com/labstack/echo"
+	"github.com/sirupsen/logrus"
 )
+
+var logger = logrus.New()
 
 func main() {
 
-	// SET UP LOGGING FILE
-	f, err := os.OpenFile("vcodeMitigationExpire_"+time.Now().Format("20060102_150405")+".log", os.O_CREATE|os.O_WRONLY, 0644)
+	var socketPath string
+	flag.StringVar(&socketPath, "socket", "/run/guest-services/backend.sock", "Unix domain socket to listen on")
+	flag.Parse()
+
+	_ = os.RemoveAll(socketPath)
+
+	logger.SetOutput(os.Stdout)
+
+	logMiddleware := middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Skipper: middleware.DefaultSkipper,
+		Format: `{"time":"${time_rfc3339_nano}","id":"${id}",` +
+			`"method":"${method}","uri":"${uri}",` +
+			`"status":${status},"error":"${error}"` +
+			`}` + "\n",
+		CustomTimeFormat: "2006-01-02 15:04:05.00000",
+		Output:           logger.Writer(),
+	})
+
+	logger.Infof("Starting listening on %s\n", socketPath)
+	router := echo.New()
+	router.HideBanner = true
+	router.Use(logMiddleware)
+	startURL := ""
+
+	ln, err := listen(socketPath)
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal(err)
 	}
-	defer f.Close()
-	log.SetOutput(f)
-	log.Printf("Started running")
+	router.Listener = ln
 
-	// SET SOME VARIABLES
-	var appSkip bool
-	var flaws []vcodeapi.Flaw
-	var recentBuild string
-	var errorCheck error
-	var flawList []string
-	var buildsBack int
+	router.GET("/scan", hello)
 
-	// PARSE CONFIG FILE AND LOG CONFIG SETTINGS
-	config := parseConfig()
-	log.Printf("Config Settings:\n"+
-		"[*] Creds File: %v\n"+
-		"[*] Target Mitigations:%v\n"+
-		"[*] Comment Text:%v\n"+
-		"[*] App Scope:%v\n"+
-		"[*] Expiration Details:%v\n",
-		config.Auth.CredsFile, config.TargetMitigations, config.CommentText, config.AppScope, config.ExpirationDetails)
+	logger.Fatal(router.Start(startURL))
+}
 
-	// GET APP LIST
-	appList := getApps(config.Auth.CredsFile, config.AppScope.LimitAppList, config.AppScope.AppListTextFile)
-	appCounter := 0
+func listen(path string) (net.Listener, error) {
+	return net.Listen("unix", path)
+}
 
-	// CYCLE THROUGH EACH APP
-	for _, appID := range appList {
-		//ADJUST SOME VARIABLES
-		flawList = []string{}
-		appSkip = false
-		appCounter++
+func hello(ctx echo.Context) error {
+	cmd := exec.Command("bash", "-c", "echo 'Hello, world!'")
 
-		fmt.Printf("Processing App ID %v (%v of %v)\n", appID, appCounter, len(appList))
+	// Execute the command and capture the output
+	output, err := cmd.CombinedOutput()
 
-		//GET THE BUILD LIST
-		buildList, err := vcodeapi.ParseBuildList(config.Auth.CredsFile, appID)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		// GET FOUR MOST RECENT BUILD IDS
-		if len(buildList) == 0 {
-			appSkip = true
-			flaws = nil
-			recentBuild = ""
-		} else {
-			//GET THE DETAILED RESULTS FOR MOST RECENT BUILD
-			flaws, _, errorCheck = vcodeapi.ParseDetailedReport(config.Auth.CredsFile, buildList[len(buildList)-1].BuildID)
-			recentBuild = buildList[len(buildList)-1].BuildID
-			buildsBack = 1
-			//IF THAT BUILD HAS AN ERROR, GET THE NEXT MOST RECENT (CONTINUE FOR 4 TOTAL BUILDS)
-			for i := 1; i < 4; i++ {
-				if len(buildList) > i && errorCheck != nil {
-					flaws, _, errorCheck = vcodeapi.ParseDetailedReport(config.Auth.CredsFile, buildList[len(buildList)-(i+1)].BuildID)
-					recentBuild = buildList[len(buildList)-(i+1)].BuildID
-					buildsBack = i + 1
-					fmt.Println(buildsBack)
-				}
-			}
-			// IF 4 MOST RECENT BUILDS HAVE ERRORS, THERE ARE NO RESULTS AVAILABLE
-			if errorCheck != nil {
-				appSkip = true
-			}
-		}
-
-		//CHECK FLAWS AND
-		if appSkip == false {
-			for _, f := range flaws {
-				// ONLY RUN ON MITIGATED REMEDIATION STATUS (TAKES INTO ACCOUNT ACCEPTED AND NOT FIXED)
-				if f.RemediationStatus == "Mitigated" || f.RemediationStatus == "Reviewed - No Action Taken" || f.RemediationStatus == "Potential False Positive" || f.RemediationStatus == "Remediated by User" {
-					//THE MOST RECENT MITIGATION ACTION IS THE ACCEPTANCE, PROPOSAL SHOULD BE SECOND LAST IN ARRAY
-					recentProposal := f.Mitigations.Mitigation[len(f.Mitigations.Mitigation)-2]
-					recentApproval := f.Mitigations.Mitigation[len(f.Mitigations.Mitigation)-1]
-					// CHECK FOR MITIGATION TYPE
-					if (recentProposal.Action == "Potential False Positive" && config.TargetMitigations.PotentialFalsePositive == true) ||
-						(recentProposal.Action == "Mitigate by Design" && config.TargetMitigations.MitigatedByDesign == true) ||
-						(recentProposal.Action == "Mitigate by Network Environment" && config.TargetMitigations.MitigatedByNetworkEnv == true) ||
-						(recentProposal.Action == "Mitigate by OS Environment" && config.TargetMitigations.MitigationByOSEnv == true) ||
-						(recentProposal.Action == "Reviewed - No Action Taken" && config.TargetMitigations.ReviewedNoActionTaken == true) ||
-						(recentProposal.Action == "Remediated by User" && config.TargetMitigations.RemediatedByUser == true) {
-						// CHECK FOR INCLUDING COMMENT TEXT
-						if (config.CommentText.RequireCommentText == true && strings.Contains(recentApproval.Description, config.CommentText.Text)) ||
-							(config.CommentText.RequireCommentText == false) {
-							// CHECK IF EXPIRED AND BUILD ARRAY
-							if expireCheck(f, config) == true {
-								flawList = append(flawList, f.Issueid)
-							}
-						}
-					}
-				}
-			}
-			// IF WE HAVE FLAWS MEETING CRITERIA, RUN UPDATE MITIGATION API
-			if len(flawList) > 0 {
-				log.Printf("[*]Trying to mitigate IDs %v in Build ID %v in App ID %v", flawList, recentBuild, appID)
-
-				if config.Mode.LogOnly == true {
-					log.Printf("App ID: %v: Flaw ID(s) %v meet criteria\n", appID, strings.Join(flawList, ","))
-				} else {
-					// TRY TO EXPIRE FLAW
-					expireError := vcodeapi.ParseUpdateMitigation(config.Auth.CredsFile, recentBuild,
-						"rejected", config.ExpirationDetails.RejectionComment, strings.Join(flawList, ","))
-					// IF WE HAVE AN ERROR, WE NEED TO TRY 2 BUILDS BACK FROM RESULTS BUILD
-					// EXAMPLE = RESULTS IN BUILD 3 (MANUAL); DYNAMIC IS BUILD 2; STATIC IS BUILD 1 (BUILD WE NEED TO MITIGATE STATIC FLAW)
-					for i := 0; i < 1; i++ {
-						if expireError != nil {
-							expireError = vcodeapi.ParseUpdateMitigation(config.Auth.CredsFile, buildList[len(buildList)-(buildsBack+i)].BuildID,
-								"rejected", config.ExpirationDetails.RejectionComment, strings.Join(flawList, ","))
-							if expireError != nil {
-								log.Printf("[*] %v", expireError)
-							}
-						}
-					}
-					// IF EXPIRE ERROR IS STILL NOT NULL, NOW WE LOG THE ERROR AND EXIT
-					if expireError != nil {
-						log.Fatalf("[!] Could not reject Flaw IDs %v in App ID %v", flawList, appID)
-					}
-					// LOG SUCCESSFUL REJECTED MITIGATIONS
-					log.Printf("App ID %v: Rejected Flaw IDs %v\n", appID, strings.Join(flawList, ","))
-				}
-			}
-		}
+	if err != nil {
+		return ctx.JSON(http.StatusNotAcceptable, HTTPMessageBody{Message: "Error"})
+	} else if output != nil {
+		return ctx.JSON(http.StatusOK, HTTPMessageBody{Message: "Hello"})
+	} else{
+		return ctx.JSON(http.StatusOK, HTTPMessageBody{Message: "Hello"})
 	}
-	log.Printf("Completed running")
+
+	
+}
+
+type HTTPMessageBody struct {
+	Message string
+}
+
+type ContainerScanResults struct {
+	TargetID        string `json:"target_id"`
+	Findings        Findings
+	Vulnerabilities Vulnerabilities
+	Secrets         Secrets
+	Configs         Configs
+	PolicyResults   []PolicyResult `json:"policy-results"`
+	PolicyPassed    bool           `json:"policy-passed"`
+}
+
+type Findings struct {
+	// Define the fields of the Findings struct here
+}
+
+type Vulnerabilities struct {
+	// Define the fields of the Vulnerabilities struct here
+}
+
+type Secrets struct {
+	// Define the fields of the Secrets struct here
+}
+
+type Configs struct {
+	// Define the fields of the Configs struct here
+}
+
+type PolicyResult struct {
+	// Define the fields of the PolicyResult struct here
 }
